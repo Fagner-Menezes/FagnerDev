2acb1d95111e2c23f4b7f8eabf9829bea7af7ac2
Fagner-Menezes <fagnermenezes2018@gmail.com>
2022-07-27 09:10:35 -0300

    commit inicial

---
+++ CursoProgramacaoNelio/src/CalculaMaiorNumero.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CalculaMaiorNumero {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter three numbers:");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		if (a > b && a > c) {
+			System.out.println("Higher = " + a);
+		} else if (b > c) {
+			System.out.println("Higher = " + b);
+		} else {
+			System.out.println("Higher = " + c);
+		}
+		sc.close();
+
+	}
+
+}
---
+++ CursoProgramacaoNelio/src/OperaçãoSplit.java
@@ -0,0 +1,13 @@
+
+public class OperaçãoSplit {
+
+	public static void main(String[] args) {
+		String s = "potato apple lemon";
+		String[] vect = s.split(" ");
+		String word1 = vect[0];
+		String word2 = vect[1];
+		String word3 = vect[2];
+
+	}
+
+}
